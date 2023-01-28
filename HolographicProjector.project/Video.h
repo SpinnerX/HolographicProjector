ce97bf3843430abad16648ff40fcde1fa871e0d5
SpinnerX <heraaronhotmail@yahoo.com>
2023-01-28 15:48:50 -0800

    Project

---
+++ HolographicProjector.project/Video.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace std;
+using namespace cv;
+
+/*
+    Handles playing videos for us.
+*/
+
+class MyVideos{
+public:
+    MyVideos(string filename, string label){
+        this->filename = filename;
+        this->label = label;
+    }
+
+    void run(){
+        VideoCapture cam(filename);
+
+        if(!cam.isOpened()){
+            cout << "Cannot find that camera index!" << endl;
+            return;
+        }
+
+        Mat frame;
+
+        while(true){
+            cam >> frame;
+
+            imshow(label, frame);
+
+            if(waitKey(30) >= 0) break;
+        }
+
+        cam.release();
+    }
+
+private:
+    string filename;
+    string label;
+};
